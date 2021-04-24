4b262a40201971182d08310a3bf861ab495c2650
Lee Formento <leesimeformento@gmail.com>
2021-04-24 01:15:39 -0500

    done with algo

---
+++ FunctionPredict.js
@@ -0,0 +1,48 @@
+// Predict 1
+
+function greeting() {
+    return "Hello";
+    console.log("World");
+}
+var word = greeting();
+console.log(word);
+
+// My Prediction "Hello";
+
+
+
+// Predict 2
+function add(num1, num2){
+    console.log("Summing Numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    return sum;
+}
+var result1 = add(3,5);
+var result2 = add(4,7);
+console.log(result1);
+console.log(result2);
+
+// My Prediction 
+// "Summing Numbers"
+// num1 is 3
+// num2 is 4
+
+
+
+
+// Predict 3
+
+function highFive(num){
+    for(var i=0; i<num; i++){
+        if(i == 5){
+            console.log("High Five!");
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+
+// My prediction Undefined.
